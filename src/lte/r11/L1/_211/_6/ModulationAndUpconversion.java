1f892a493e167aab30c149bc3c1e83894b1f2b03
Michael Duo Ling <duo.ling.cn@gmail.com>
2013-11-24 20:42:07 +0100

    OFDM

---
+++ src/lte/r11/L1/_211/_6/ModulationAndUpconversion.java
@@ -0,0 +1,53 @@
+package lte.r11.L1._211._6;
+
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealVector;
+
+import common.math.ComplexVector;
+
+public class ModulationAndUpconversion extends common.BasicBaseClass {
+
+	static RealVector downlinkModulation(ComplexVector signal,
+			double timeStart, double timeStep, double f__0) throws Exception {
+		RealVector result = new ArrayRealVector();
+		double t = timeStart;
+		for (Complex symbol : signal.toArray()) {
+			result = result.append(symbol.getReal()
+					* Math.cos(2 * Math.PI * f__0 * t) - symbol.getImaginary()
+					* Math.sin(2 * Math.PI * f__0 * t));
+			t += timeStep;
+		}
+		return result;
+	}
+
+	static RealVector downlinkModulation(ComplexVector signal,
+			RealVector timeVector, double f__0) throws Exception {
+		garantee(signal.getDimension() == timeVector.getDimension(),
+				"Input signal must have the same length as time vector!!");
+		RealVector result = new ArrayRealVector();
+		for (int i = 0; i < signal.getDimension(); i++) {
+			result = result.append(signal.getEntry(i).getReal()
+					* Math.cos(2 * Math.PI * f__0 * timeVector.getEntry(i))
+					- signal.getEntry(i).getImaginary()
+					* Math.sin(2 * Math.PI * f__0 * timeVector.getEntry(i)));
+		}
+		return result;
+	}
+
+	@Override
+	public String toBinaryString() {
+		return toReadableString();
+	}
+
+	@Override
+	public String toReadableString() {
+		return ModulationAndUpconversion.class.getName();
+	}
+
+	@Override
+	public String toString() {
+		return toReadableString();
+	}
+
+}
---
+++ src/lte/r11/L1/_211/_6/OfdmBaseBandSignalGeneration.java
@@ -0,0 +1,23 @@
+package lte.r11.L1._211._6;
+
+public class OfdmBaseBandSignalGeneration extends common.BasicBaseClass {
+
+	@Override
+	public String toBinaryString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toReadableString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
